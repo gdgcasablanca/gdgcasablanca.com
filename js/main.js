321530a511fbf0c81a8693b7ca7e34b452e2a92f
Omar Houmz <omhoumz@gmail.com>
2018-12-29 12:18:34 -0800

    Rewrites card component to native javascript instead of template literal * Adds styles to the see all link * Adds crowler delay

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
-const DB_ENDPOINT = 'https://gdgcasablanca-website.firebaseio.com/events.json'
+const DB_ENDPOINT = 'https://gdgcasablanca-website.firebaseio.com/events.json';
 
+document.addEventListener("DOMContentLoaded", function () {
+  onDomContentLoaded();
+});
 
 function onDomContentLoaded () {
-  const eventCardsDiv = document.querySelector('.event-cards')
+  const eventCardsDiv = document.querySelector('.event-cards');
 
   fetch(DB_ENDPOINT)
     .then(response => response.json())
     .then(events => {
       const allEventCards = events.map(evnt => {
-        const title = evnt.title
-        const date = evnt.date
-        const timeFrom = evnt.timeFrom
-        const timeTo = evnt.timeTo
-        const location = evnt.location
-        const eventLink = evnt.eventLink
-
-        return eventComponent({title, date, timeFrom, timeTo, location, eventLink})
+        return eventComponent({
+          title: evnt.title,
+          date: evnt.date,
+          timeFrom: evnt.timeFrom,
+          timeTo: evnt.timeTo,
+          location: evnt.location,
+          eventLink: evnt.eventLink
+        });
       })
-      eventCardsDiv.innerHTML = allEventCards
+
+      appendChildren({parentNode:eventCardsDiv, children: allEventCards});
     })
 }
 
@@ -29,19 +33,58 @@ function eventComponent ({
   location,
   eventLink
 }) {
-  return `
-  <div class="event-card">
-    <h3 class="title">${title}</h3>
-    <ul class="details">
-      <li><i class="material-icons">calendar_today</i> ${date}</li>
-      <li><i class="material-icons">access_time</i> ${timeFrom} to ${timeTo}</li>
-      <li><i class="material-icons">pin_drop</i> ${location}</li>
-    </ul>
-    <a class="more-link" href="${eventLink}" target="_blank" rel="noopener noreferrer">Check Event page</a>
-  </div>
-  `;
+  const card = createElement({element: 'div', className: 'event-card'});
+
+  const heading = createElement({element: 'h3', className: 'title', innerText: title});
+
+  const detailsList = [{
+    icon: 'calendar_today',
+    text: ` ${date}`
+  }, {
+    icon: 'access_time',
+    text: ` ${timeFrom} to ${timeTo}`
+  }, {
+    icon: 'pin_drop',
+    text: ` ${location}`
+  }];
+  const detailsListElements = detailsList.map(list => createListItem({icon: list.icon, text: list.text}));
+  const details = createElement({element: 'ul', className: 'details'});
+  appendChildren({parentNode: details, children: detailsListElements});
+
+  const anchor = createElement({element: 'a', className: 'more-link', innerText: 'Check Event page'});
+  anchor.setAttribute('href', eventLink);
+  anchor.setAttribute('target', '_blank');
+  anchor.setAttribute('rel', 'noopener noreferrer');
+
+  appendChildren({parentNode: card, children: [ heading, details, anchor ]});
+
+  return card;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  onDomContentLoaded()
-});
+function createElement ({ element, className = null, innerText = null }) {
+  var domElement = document.createElement(element);
+  if (className) domElement.setAttribute('class', className);
+  if (innerText) domElement.innerText = innerText;
+
+  return domElement;
+}
+
+function createListItem ({icon, text}) {
+  const materialIcon = createElement({element: 'i', className: 'material-icons'});
+  materialIcon.innerText = icon;
+
+  const textNode = document.createTextNode(text);
+  
+  const listItem = createElement({element: 'li'});
+
+  appendChildren({parentNode: listItem, children: [materialIcon, textNode]});
+
+  return listItem;
+}
+
+function appendChildren ({parentNode = null, children = null}) {
+  if (!parentNode || !children) return;
+  children.forEach(child => {
+    parentNode.appendChild(child)
+  })
+}
