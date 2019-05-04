d398ee3a995607f97f339e8e7bccdfd25a0c9e78
Omar Houmz <omar.houmz@schibsted.com>
2019-05-04 13:35:29 +0100

    add id prop to base section

--- src/components/base-section/base-section.js
+++ src/components/base-section/base-section.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 import './base-section.css'
 
-export const BaseSection = ({ children, sectionTitle, classNames }) => {
+export const BaseSection = ({ children, sectionTitle, classNames, id }) => {
   return (
-    <section className={'section ' + classNames}>
+    <section className={'section ' + classNames} id={id}>
       <div className="container">
         <h1
           className="heading"
