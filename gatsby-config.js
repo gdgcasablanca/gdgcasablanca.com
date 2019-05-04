01457871755f1da5a053a81c4e28b14fb005884b
Omar Houmz <omar.houmz@schibsted.com>
2019-05-04 11:59:10 +0100

    edit gatsby manifest config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `GDG Casablanca`,
+        short_name: `GDG Casa`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1a73e8`,
+        theme_color: `#1a73e8`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gdg-casa-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
