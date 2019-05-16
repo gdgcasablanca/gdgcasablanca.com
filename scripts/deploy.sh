f1135ef84fd5ef4decac08fc2c8fb3da8ef9f340
Omar Houmz <omar.houmz@schibsted.com>
2019-05-16 01:06:18 +0000

    make sure that this works

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-echo 'DEPLOYING ####'
-echo $TRAVIS_BRANCH
+echo 'DEPLOYING IN: '
+
+firebase use develop
+
+if [ "$TRAVIS_BRANCH" = "develop" ]; then
+  echo 'develop'
+  firebase use develop
+elif [ "$TRAVIS_BRANCH" = "master" ]
+  echo 'master'
+  firebase use default
+fi
+
+firebase deploy --token $FIREBASE_TOKEN
