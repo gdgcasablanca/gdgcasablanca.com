0e4c0c570e701f9bc2cc49b5918034f33c6fd430
Omar Houmz <om.houmz@gmail.com>
2019-07-15 00:40:33 +0100

    refactor: landmarks in header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,32 +8,30 @@ import './header.css'
 
 export const Header = ({ siteTitle }) => (
   <header className="header">
-    <nav className="nav">
-      <div className="container">
-        <Link to="/" className="logo logo-gdg">
-          <img src={GDGCasaLogo} alt={siteTitle} />
-        </Link>
+    <div className="container">
+      <Link to="/" className="logo logo-gdg">
+        <img src={GDGCasaLogo} alt={siteTitle} />
+      </Link>
 
-        <div className="nav-links">
-          <Link to="/">Home</Link>
-          <Link to="/#Events">Events</Link>
-          <Link to="/#Contact">Contact</Link>
-          <Link to="/team">Team</Link>
-          {/* <Link to="#!">About</Link>  */}
-        </div>
+      <nav className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/#Events">Events</Link>
+        <Link to="/#Contact">Contact</Link>
+        <Link to="/team">Team</Link>
+        {/* <Link to="#!">About</Link>  */}
+      </nav>
 
-        <Link
-          to="https://wtm.gdgcasablanca.com"
-          className="logo logo-wtm"
-          external
-        >
-          <img
-            src={WTMCasaLogo}
-            alt="WTM Casablanca. Connect. Create. Celebrate."
-          />
-        </Link>
-      </div>
-    </nav>
+      <Link
+        to="https://wtm.gdgcasablanca.com"
+        className="logo logo-wtm"
+        external
+      >
+        <img
+          src={WTMCasaLogo}
+          alt="WTM Casablanca. Connect. Create. Celebrate."
+        />
+      </Link>
+    </div>
   </header>
 )
 
