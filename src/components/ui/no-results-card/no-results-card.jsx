2c797f607cc90e3c2b4e0263a25bac1233beb13c
Omar Houmz <omhoumz@gmail.com>
2020-06-26 21:49:09 +0100

    feat: add loading and no results cards

---
+++ src/components/ui/no-results-card/no-results-card.jsx
@@ -0,0 +1,13 @@
+import React, { memo } from 'react'
+
+import styles from './no-results-card.module.css'
+
+const NoResultsCard = memo(function NoResultsCard(props) {
+  return (
+    <div className={styles.noResultsCard}>
+      <h3 className={styles.title}>There is no planned for now.</h3>
+    </div>
+  )
+})
+
+export default NoResultsCard
