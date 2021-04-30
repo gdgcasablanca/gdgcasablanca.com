81a702ae874a364817dd304869e764b11339089f
Omar Houmz <omarhoumz@gmail.com>
2021-04-30 23:21:05 +0000

    chore: update packages & main branch name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,8 @@ yarn build
 if [ "$TRAVIS_BRANCH" = "develop" ]; then
   echo 'Deploying to the develop branch ...'
   firebase use develop
-elif [ "$TRAVIS_BRANCH" = "master" ]; then
-  echo 'Deploying to the master branch ...'
+elif [ "$TRAVIS_BRANCH" = "main" ]; then
+  echo 'Deploying to the main branch ...'
   firebase use default
 fi
 
