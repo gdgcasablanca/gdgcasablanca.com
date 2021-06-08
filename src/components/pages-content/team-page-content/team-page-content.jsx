2cf6f54919abbe6d0078add3b9b13a2267228464
Omar Houmz <omarhoumz@gmail.com>
2021-06-08 23:47:47 +0100

    Develop (#41)
    * Align master with dev (#39)
    
    * chore: update packages & main branch name
    
    * chore: remove old code
    
    * chore: add team members page (#40)

--- src/components/pages-content/team-page-content/team-page-content.jsx
+++ src/components/pages-content/team-page-content/team-page-content.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
+import * as React from 'react'
+import useSWR from 'swr'
 
 import Link from '../../link/link'
 import BaseSection from '../../base-section/base-section'
 import OrganizerList from '../../organizer-list/organizer-list'
 
-import teamMembersData from './team-members-data'
 import Text from '../../ui/text/text'
 
 const TeamPageContent = () => {
-  return (
-    <>
+  const { data, error } = useSWR(
+    'https://gdgcasablanca-admin.vercel.app/api/members',
+  )
+
+  if (error)
+    return (
       <BaseSection SectionTitle='Team.'>
-        <OrganizerList teamMembersData={teamMembersData} />
-        <Text>
-          Contact us via email: &nbsp;
-          <Link to='mailto:contact@gdgcasablanca.com' external>
-            contact@gdgcasablanca.com
-          </Link>
-        </Text>
+        <div>failed to load</div>
       </BaseSection>
-    </>
+    )
+  if (!data)
+    return (
+      <BaseSection SectionTitle='Team.'>
+        <div>loading...</div>
+      </BaseSection>
+    )
+
+  const teamMembersData = data.map((member) => ({
+    name: `${member.firstname} ${member.lastname}`,
+    imgLink: member.photo,
+    job: member.occupation,
+    link: member.url ?? '#!',
+    id: member.id,
+  }))
+
+  return (
+    <BaseSection SectionTitle='Team.'>
+      <OrganizerList teamMembersData={teamMembersData} />
+
+      <Text>
+        Contact us via email: &nbsp;
+        <Link to='mailto:contact@gdgcasablanca.com' external>
+          contact@gdgcasablanca.com
+        </Link>
+      </Text>
+    </BaseSection>
   )
 }
 
